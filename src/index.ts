863de9edceea9c392afd035bbd467ec2a15174ca
sergey.krasnov <kaesve@yandex.ru>
2026-09-21 13:51:44 +0700

    fix: paths fixed

--- src/index.ts
+++ src/index.ts
@@ -8,71 +8,67 @@ import { MetaPluginOption } from './types';
 import { buildError, errorStack, greenText, redText } from './utils';
 
 export type PluginConfig = {
-	readonly command: 'build' | 'serve';
-	readonly logger: Logger;
-	readonly publicDir: string;
-	readonly outDir: string;
+  readonly command: 'build' | 'serve';
+  readonly logger: Logger;
+  readonly publicDir: string;
+  readonly outDir: string;
 };
 
 function createError(err: unknown): string {
-	process.exitCode = 1;
-	const error = buildError(err);
-	return `\n${redText(error.message)}${errorStack(error)}`;
+  process.exitCode = 1;
+  const error = buildError(err);
+  return `\n${redText(error.message)}${errorStack(error)}`;
 }
 
 type MetaPluginConfig = Partial<MetaPluginOption> & {
-	readonly convert?: boolean;
-	readonly audioDuration?: boolean;
+  readonly convert?: boolean;
+  readonly audioDuration?: boolean;
 };
 
 const metaPlugin = (pluginConfig: MetaPluginConfig = {}): PluginOption => {
-	const { convert = true, audioDuration = true, ...options } = pluginConfig;
-	const plugin = new MetaPlugin({
-		version: process.env['GAME_VERSION'],
-		...options,
-	});
+  const { convert = true, audioDuration = true, ...options } = pluginConfig;
+  const plugin = new MetaPlugin({
+    version: process.env['GAME_VERSION'],
+    ...options,
+  });
 
-	let config: PluginConfig;
+  let config: PluginConfig;
 
-	return {
-		name: 'vite-plugin-meta',
-		async config(viteConfig, env) {
-			if (env.command !== 'serve' || viteConfig.publicDir === false) return;
+  return {
+    name: 'vite-plugin-meta',
+    async configResolved(viteConfig) {
+      const sep = viteConfig.publicDir.includes(path.win32.sep) ? path.win32.sep : path.posix.sep;
+      config = {
+        command: viteConfig.command,
+        logger: viteConfig.logger,
+        outDir: viteConfig.build.outDir,
+        publicDir: viteConfig.publicDir.split(sep).at(-1) ?? '',
+      };
+      try {
+        plugin.selectFiles(config.publicDir);
+        if (audioDuration) await plugin.audioDurationProcess();
+        await plugin.writeConfig(false, config.publicDir);
+      } catch (err) {
+        config.logger.error(createError(err));
+      }
+    },
 
-			const root = viteConfig.root ?? process.cwd();
-			const publicDir = path.resolve(root, viteConfig.publicDir ?? 'public');
-			try {
-				plugin.selectFiles(publicDir);
-				if (audioDuration) await plugin.audioDurationProcess();
-				await plugin.writeConfig(false, publicDir);
-			} catch (err) {
-				config.logger.error(createError(err));
-			}
-		},
-		configResolved(viteConfig) {
-			config = {
-				command: viteConfig.command,
-				logger: viteConfig.logger,
-				outDir: viteConfig.build.outDir,
-				publicDir: viteConfig.publicDir,
-			};
-		},
-		async closeBundle() {
-			if (config.command !== 'build') return;
-			const { logger, outDir } = config;
-			logger.info(`\n${greenText('Metaprocesses started')}`);
-			try {
-				if (convert) await plugin.convertProcess(config.publicDir);
-				plugin.selectFiles(outDir);
-				if (audioDuration) await plugin.audioDurationProcess();
-				await plugin.writeConfig(convert, outDir);
-				if (convert) await plugin.transferProcess();
-				logger.info(`${greenText('✓')} metaprocesses completed\n`);
-			} catch (err) {
-				logger.error(createError(err));
-			}
-		},
-	};
+    async closeBundle() {
+      if (config.command !== 'build') return;
+      const { logger, outDir } = config;
+      logger.info(`\n${greenText('Metaprocesses started')}`);
+      try {
+        if (convert) await plugin.convertProcess(config.publicDir);
+        plugin.selectFiles(outDir);
+        if (audioDuration) await plugin.audioDurationProcess();
+        if (convert) await plugin.transferProcess();
+        await plugin.writeConfig(convert, outDir);
+        logger.info(`${greenText('✓')} metaprocesses completed\n`);
+      } catch (err) {
+        logger.error(createError(err));
+      }
+    },
+  };
 };
 
 export default metaPlugin;
